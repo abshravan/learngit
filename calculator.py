a538d0c5a664c31bbb487de06986b5d802830385
A B Shravan Krishna <abshravankrishna@gmail.com>
2023-09-22 20:39:59 +0530

    added gui to calculator

--- calculator.py
+++ calculator.py
@@ -1,43 +1,73 @@
-# Define the calculator function
-def calculator():
-    print("Simple Calculator")
-    print("Options:")
-    print("Enter 'add' for addition")
-    print("Enter 'subtract' for subtraction")
-    print("Enter 'multiply' for multiplication")
-    print("Enter 'divide' for division")
-    print("Enter 'quit' to end the program")
+import tkinter as tk
 
-    while True:
-        # Take user input
-        user_input = input(": ")
+# Define functions for calculator operations
+def add():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        result.set(num1 + num2)
+    except ValueError:
+        result.set("Invalid input")
 
-        # Check if the user wants to quit
-        if user_input == "quit":
-            break
-        elif user_input not in ["add", "subtract", "multiply", "divide"]:
-            print("Invalid input. Please enter a valid operation.")
-            continue
+def subtract():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        result.set(num1 - num2)
+    except ValueError:
+        result.set("Invalid input")
 
-        # Get numbers from the user
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+def multiply():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        result.set(num1 * num2)
+    except ValueError:
+        result.set("Invalid input")
 
-        # Perform the selected operation
-        if user_input == "add":
-            result = num1 + num2
-        elif user_input == "subtract":
-            result = num1 - num2
-        elif user_input == "multiply":
-            result = num1 * num2
-        elif user_input == "divide":
-            if num2 == 0:
-                print("Division by zero is not allowed.")
-                continue
-            result = num1 / num2
+def divide():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        if num2 == 0:
+            result.set("Division by zero")
+        else:
+            result.set(num1 / num2)
+    except ValueError:
+        result.set("Invalid input")
 
-        # Display the result
-        print("Result: " + str(result))
+# Create the main window
+root = tk.Tk()
+root.title("Simple Calculator")
 
-# Run the calculator
-calculator()
+# Create input fields and labels
+label_num1 = tk.Label(root, text="Enter first number:")
+label_num1.pack()
+entry_num1 = tk.Entry(root)
+entry_num1.pack()
+
+label_num2 = tk.Label(root, text="Enter second number:")
+label_num2.pack()
+entry_num2 = tk.Entry(root)
+entry_num2.pack()
+
+# Create buttons for calculator operations
+btn_add = tk.Button(root, text="Add", command=add)
+btn_add.pack()
+
+btn_subtract = tk.Button(root, text="Subtract", command=subtract)
+btn_subtract.pack()
+
+btn_multiply = tk.Button(root, text="Multiply", command=multiply)
+btn_multiply.pack()
+
+btn_divide = tk.Button(root, text="Divide", command=divide)
+btn_divide.pack()
+
+# Create a label to display the result
+result = tk.StringVar()
+result_label = tk.Label(root, textvariable=result)
+result_label.pack()
+
+# Run the GUI application
+root.mainloop()
